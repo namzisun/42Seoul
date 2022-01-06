b8edded17925784ef86375eac0d461246f8b3d18
namzisun <namzisun@gmail.com>
2022-01-06 17:25:04 +0900

    Add b_zero, calloc, memset

--- circle_0/libft/ft_calloc.c
+++ circle_0/libft/ft_calloc.c
@@ -3,11 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jnam <jnam@student.42seoul.kr>             +#+  +:+       +#+        */
+/*   By: jnam <jnam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 16:59:12 by jnam              #+#    #+#             */
-/*   Updated: 2022/01/04 16:59:14 by jnam             ###   ########.fr       */
+/*   Updated: 2022/01/06 17:23:32 by jnam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void    *calloc(size_t count, size_t size)
+{
+    char    *str;
+    int     i;
+
+    i = 0;
+    str = malloc((count) * size);
+    while (i < (count * size))
+    {
+        *(str + i) = 0;
+        i++;
+    }
+    return (str);
+}
--- circle_0/libft/ft_memset.c
+++ circle_0/libft/ft_memset.c
@@ -3,11 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jnam <jnam@student.42seoul.kr>             +#+  +:+       +#+        */
+/*   By: jnam <jnam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 15:10:47 by jnam              #+#    #+#             */
-/*   Updated: 2022/01/03 15:10:48 by jnam             ###   ########.fr       */
+/*   Updated: 2022/01/06 17:18:24 by jnam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+// 어떤 메모리의 시작점부터 연속된 범위를 어떤 값으로(바이트 딘위) 지정
+void    *ft_memset(void *dest, int value, size_t len)
+{
+    size_t          i;
+    unsigned char   *tmp;
+
+    tmp = (unsigned char *)dest;
+    i = 0;
+    while (i < len)
+        tmp[i++] = (unsigned char) value;
+    return (tmp);
+}
