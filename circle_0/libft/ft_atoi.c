501b705487ddb44cdd9ba448bb6bd362ea61eddf
namzisun <namzisun@gmail.com>
2022-01-11 18:08:41 +0900

    Add atoi func

--- circle_0/libft/ft_atoi.c
+++ circle_0/libft/ft_atoi.c
@@ -11,3 +11,40 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int ft_isspace(char c)
+{
+    if (c == ' ' || c == '\n' || c == '\t' ||
+                    c == '\v' || c == '\f' || c == '\r')
+        return (1);
+    else
+        return (0);
+}
+
+int ft_atoi(const char *str)
+{
+    int     i;
+    long    sign;
+    long    num;
+
+    i = 0;
+    sign = 1;
+    num = 0;
+    while (str[i] != '\0' && ft_isspace(str[i]))
+        i++;
+    if (str[i++] == '-')
+        sign = -1;
+    else if (str[i] == '+')
+        i++;
+    while (str[i] != '\0' && ('0' <= str[i] && str[i] <= '9'))
+    {
+        num *= 10;
+        num += (str[i] - '0');
+        if (num > 2147483647 && sign == 1)
+            return (-1);
+        if (num > 2147483648 && sign == -1)
+            return (0);
+        i++;
+    }
+    return (num * sign);
+}
