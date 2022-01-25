c154d778c5b2aac262371be8bdf8dbc07b29c8e1
namzisun <namzisun@gmail.com>
2022-01-25 23:21:00 +0900

    Update isalpha func

--- circle_0/libft/ft_isalpha.c
+++ circle_0/libft/ft_isalpha.c
@@ -6,17 +6,15 @@
 /*   By: jnam <jnam@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 17:33:48 by jnam              #+#    #+#             */
-/*   Updated: 2022/01/23 18:47:49 by jnam             ###   ########.fr       */
+/*   Updated: 2022/01/25 23:18:34 by jnam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_lsalpha(int c)
+int	ft_isalpha(int c)
 {
-	if ('A' <= c && c <= 'Z')
+	if (('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z'))
 		return (1);
-	else if ('a' <= c && c <= 'z')
-		return (2);
 	return (0);
 }
