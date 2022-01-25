e9db5a3366f82799ba4fccdddd211d9ccd262bc3
namzisun <namzisun@gmail.com>
2022-01-25 23:53:46 +0900

    Update strncmp

--- circle_0/libft/ft_strncmp.c
+++ circle_0/libft/ft_strncmp.c
@@ -6,18 +6,25 @@
 /*   By: jnam <jnam@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 17:39:10 by jnam              #+#    #+#             */
-/*   Updated: 2022/01/25 16:55:11 by jnam             ###   ########.fr       */
+/*   Updated: 2022/01/25 23:53:22 by jnam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *str1, const char *str2, size_t size)
+int	ft_strncmp(const char *s1, const char *s2, size_t size)
 {
 	size_t	i;
 
 	i = 0;
-	while (str1[i] == str2[i] && (i + 1) < size)
-		i++;
-	return (str1[i] - str2[i]);
+	if (size == 0)
+		return (0);
+	while (i < size - 1)
+	{
+		if (s1[i] && s2[i] && s1[i] == s2[i])
+			i++;
+		else
+			break ;
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
