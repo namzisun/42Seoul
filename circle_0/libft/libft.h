7c2ddd7b5b9b2ae8a16da8f032fee8aabe38b222
Jisun Nam <jnam@c6r5s4.42seoul.kr>
2022-01-25 17:55:02 +0900

    Update libft.h

--- circle_0/libft/libft.h
+++ circle_0/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jnam <jnam@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 16:09:33 by jnam              #+#    #+#             */
-/*   Updated: 2022/01/23 18:21:17 by jnam             ###   ########.fr       */
+/*   Updated: 2022/01/25 16:45:28 by jnam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,13 +45,13 @@ size_t	ft_strlen(const char *s);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
-void	*ft_bzero(void *dest, size_t len);
+void	ft_bzero(void *dest, size_t len);
 void	*ft_calloc(size_t count, size_t size);
 void	*ft_memchr(const void *ptr, int value, size_t size);
 void	*ft_memcpy(void *dest, const void *src, size_t size);
 void	*ft_memmove(void *dest, const void *src, size_t size);
 void	*ft_memset(void *dest, int value, size_t len);
-int		*ft_memcmp(const void *dest, const void *src, size_t size);
+int		ft_memcmp(const void *dest, const void *src, size_t size);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr_fd(char *s, int fd);
