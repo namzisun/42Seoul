2bd3fc0c999bb2a5da29a47559587953aed41a70
namzisun <namzisun@gmail.com>
2022-01-26 00:49:01 +0900

    Update atoi

--- circle_0/libft/ft_atoi.c
+++ circle_0/libft/ft_atoi.c
@@ -6,13 +6,13 @@
 /*   By: jnam <jnam@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 17:33:04 by jnam              #+#    #+#             */
-/*   Updated: 2022/01/23 19:05:22 by jnam             ###   ########.fr       */
+/*   Updated: 2022/01/26 00:48:07 by jnam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isspace(char c)
+static int	ft_isspace(char c)
 {
 	if (c == ' ' || c == '\n' || c == '\t'
 		|| c == '\v' || c == '\f' || c == '\r')
@@ -21,21 +21,35 @@ int	ft_isspace(char c)
 		return (0);
 }
 
+static long	ft_sign(const char *str, size_t *i)
+{
+	long	sign;
+	size_t	j;
+
+	j = *i;
+	sign = 1;
+	if (str[j] == '-')
+	{
+		sign = -1;
+		j++;
+	}
+	else if (str[j] == '+')
+		j++;
+	*i = j;
+	return (sign);
+}
+
 int	ft_atoi(const char *str)
 {
-	int		i;
+	size_t	i;
 	long	sign;
 	long	num;
 
 	i = 0;
-	sign = 1;
 	num = 0;
-	while (str[i] != '\0' && ft_isspace(str[i]))
+	while (str[i] && ft_isspace(str[i]))
 		i++;
-	if (str[i++] == '-')
-		sign = -1;
-	else if (str[i] == '+')
-		i++;
+	sign = ft_sign(str, &i);
 	while (str[i] != '\0' && ('0' <= str[i] && str[i] <= '9'))
 	{
 		num *= 10;
