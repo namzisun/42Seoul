4dc1a8ab5f7bf369b3ce7878f7c0233d2082a52d
namzisun <namzisun@gmail.com>
2022-01-29 22:21:01 +0900

    Update split

--- circle_0/libft/ft_split.c
+++ circle_0/libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jnam <jnam@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 15:57:41 by jnam              #+#    #+#             */
-/*   Updated: 2022/01/25 16:04:01 by jnam             ###   ########.fr       */
+/*   Updated: 2022/01/29 22:16:10 by jnam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,18 @@ static int	ft_len(char const *s, char c)
 	int	i;
 	int	count;
 
-	i = -1;
+	i = 0;
 	count = 0;
-	while (s[++i])
+	while (s[i])
 	{
-		if (s[i] == c)
-			continue ;
-		count++;
+		if (s[i] != c)
+		{
+			count++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+		else
+			i++;
 	}
 	return (count);
 }
@@ -48,17 +53,17 @@ static int	ft_word(char const *s, char c, int *j)
 
 static char	*ft_sep(char const *s, char c, int *j)
 {
-	int		i;
-	int		k;
-	int		word_len;
+	int	i;
+	int	k;
+	int	word_len;
 	char	*sep;
 
-	i = *j;
-	k = 0;
 	word_len = ft_word(s, c, j);
 	sep = (char *)malloc(sizeof(char) * (word_len + 1));
 	if (!sep)
 		return (NULL);
+	i = *j;
+	k = 0;
 	while (s[i] && s[i] != c)
 	{
 		sep[k] = s[i];
@@ -83,9 +88,9 @@ static void	ft_free(char **tab, int index)
 char	**ft_split(char const *s, char c)
 {
 	char	**tab;
-	int		len;
-	int		i;
-	int		j;
+	int	len;
+	int	i;
+	int	j;
 
 	i = 0;
 	j = 0;
