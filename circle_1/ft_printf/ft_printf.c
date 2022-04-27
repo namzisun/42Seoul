8aa878b0bd2480d981426a236d6730b3538145c6
namzisun <namzisun@gmail.com>
2022-04-28 01:47:44 +0900

    update main func

--- circle_1/ft_printf/ft_printf.c
+++ circle_1/ft_printf/ft_printf.c
@@ -6,33 +6,33 @@
 /*   By: jnam <jnam@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 20:58:42 by jnam              #+#    #+#             */
-/*   Updated: 2022/04/27 20:39:41 by jnam             ###   ########.fr       */
+/*   Updated: 2022/04/28 01:47:20 by jnam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_print_format(const char type, va_list ap)
+int	ft_printf_format(const char type, va_list ap)
 {
-    int	len;
+	int	len;
 
 	if (type == 'c')
-		len = ft_printf_c(ap);
+		len = ft_printf_char((unsigned char)va_arg(ap, int));
 	else if (type == 's')
-		len = ft_printf_s(ap);
+		len = ft_printf_str((unsigned char*)va_arg(ap, char*));
 	else if (type == 'p')
 	{
 		len = write(1, "0x", 2);
-		len += ft_printf_hex((unsigned long long)(va_arg(ap, void *)), 0, 0);
+		len += ft_printf_hex((unsigned long long)(va_arg(ap, void *)), 0);
 	}
 	else if (type == 'd' || type == 'i')
-		len = ft_printf_di(ap);
+		len = ft_printf_nbr((int)va_arg(ap, int));
 	else if (type == 'u')
-		len = ft_printf_hex((unsigned int)(va_arg(ap, int), 1, 0);
+		len = ft_printf_ui((unsigned int)(va_arg(ap, int));
 	else if (type == 'x')
-		len = ft_printf_hex((unsigned int)(va_arg(ap, int), 0, 0);
+		len = ft_printf_hex((unsigned int)(va_arg(ap, int), 0);
 	else if (type = 'X')
-		len = ft_printf_hex((unsigned int)(va_arg(ap, int), 0, 1);
+		len = ft_printf_hex((unsigned int)(va_arg(ap, int), 1);
 	else if (type == '%')
 		len = write(1, '%', 1);
 	else
@@ -52,7 +52,7 @@ int	ft_printf(const char *format, ...)
 		if (*format == '%')
 		{
 			format++;
-			len += ft_print_format(*format, ap);
+			len += ft_printf_format(*format, ap);
 		}
 		else
 		{
