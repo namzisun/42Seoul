f9ccc63b5baa7548938fc0852dd26c2042c15944
namzisun <namzisun@gmail.com>
2022-04-28 01:48:16 +0900

    add format utils func

---
+++ circle_1/ft_printf/ft_printf_utils.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jnam <jnam@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/28 00:40:02 by jnam              #+#    #+#             */
+/*   Updated: 2022/04/28 01:46:17 by jnam             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_printf_char(unsigned char c)
+{
+	int		len;
+
+	len = write(1, &c, 1);
+	return (len);
+}
+
+int	ft_printf_str(unsigned char* str)
+{
+	int		len;
+
+	if (!str)
+		ft_printf_str("(null)");
+	len = write(1, str, ft_strlen(str));
+	return (len);
+}
+
+int	ft_printf_nbr(int num)
+{
+	int	len;
+	char	*str;
+
+	str = ft_itoa(num);
+	if (!str)
+		return (INT_MIN);
+	len = write(1, str, ft_strlen(str));
+	return (len);
+}
+
+int	ft_printf_ui(unsigned int num)
+{
+	int	len;
+
+	if (num > 9)
+	{
+		len = ft_printf_nbr(num / 10);
+		len += ft_printf_char(num % 10 + '0');
+	}
+	else
+		len = ft_printf_char(num + '0');
+	return (len);
+}
+
+int	ft_printf_hex(unsigned long long num, int upper)
+{
+	int	len;
+	static char*	lower_base[16] = "0123456789abcdef";
+	static char*	upper_base[16] = "0123456789ABCDEF";
+
+	len = 0;
+	if (num < 16)
+	{
+		if (upper)
+			len += ft_printf_char(upper_base[num]);
+		else
+			len += ft_printf_char(lower_base[num]);
+	}
+	else
+	{
+		len += ft_printf_hex(num / 16, upper);
+		len += ft_printf_hex(num % 16, upper);
+	}	
+	return (len);
+}
