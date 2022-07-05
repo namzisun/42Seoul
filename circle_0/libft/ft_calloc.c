60e27499f1e3ac040f3654da1ea19e477ea6cb48
namzisun <namzisun@gmail.com>
2022-07-05 17:27:17 +0900

    update calloc

--- circle_0/libft/ft_calloc.c
+++ circle_0/libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: jnam <jnam@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 17:33:18 by jnam              #+#    #+#             */
-/*   Updated: 2022/01/25 16:41:15 by jnam             ###   ########.fr       */
+/*   Updated: 2022/07/05 17:26:57 by jnam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,12 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	char	*str;
-	size_t	i;
 
-	i = 0;
+	if (count == SIZE_MAX && size == SIZE_MAX)
+		return (NULL);
 	str = malloc((count) * size);
-	while (i < (count * size))
-	{
-		*(str + i) = 0;
-		i++;
-	}
+	if (!str)
+		return (NULL);
+	ft_memset(str, 0, size * count);
 	return (str);
 }
