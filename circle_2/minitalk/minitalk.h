d0950f2db943558b8404698b0b1476fe79a8eeee
Jisun Nam <namzisun@gmail.com>
2022-07-10 13:45:41 +0900

    norm

--- circle_2/minitalk/minitalk.h
+++ circle_2/minitalk/minitalk.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jnam <jnam@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: jnam <jnam@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/10 00:29:24 by jnam              #+#    #+#             */
-/*   Updated: 2022/07/10 01:28:22 by jnam             ###   ########.fr       */
+/*   Created: 2022/07/10 13:44:20 by jnam              #+#    #+#             */
+/*   Updated: 2022/07/10 13:45:19 by jnam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 
 void	ft_putnbr(int n);
 void	ft_putstr(char *s);
-int	ft_atoi(const char *str);
+int		ft_atoi(const char *str);
 char	*ft_itoa(int n);
 
 #endif
